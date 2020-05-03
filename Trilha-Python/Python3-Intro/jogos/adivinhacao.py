67b9eb1d67db87b0ce053bf921fffcb5dc641a61
Wellington Ribeiro <ligton.ribeiro@gmail.com>
2020-05-03 20:39:04 -0300

    Atualizações...

--- Trilha-Python/Python3-Intro/jogos/adivinhacao.py
+++ Trilha-Python/Python3-Intro/jogos/adivinhacao.py
@@ -8,7 +8,7 @@ numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
-    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    print(f"Tentativa {rodada} de {total_de_tentativas}")
 
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
