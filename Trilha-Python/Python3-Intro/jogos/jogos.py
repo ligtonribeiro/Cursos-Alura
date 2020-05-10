afb02be89774ed93b3c35747cbff8d95388c389d
Wellington Ribeiro <ligton.ribeiro@gmail.com>
2020-05-10 13:10:30 -0300

    Conclusão do Curso

---
+++ Trilha-Python/Python3-Intro/jogos/jogos.py
@@ -0,0 +1,22 @@
+import forca
+import adivinhacao
+
+def escolha_jogos():
+    print("************************")
+    print("*- Escolha o seu jogo -*")
+    print("************************")
+
+    print("(1) Forca (2) Adivinhação")
+
+    jogo = int(input("Qual jogo? "))
+
+    if jogo == 1:
+        print("Jogando Forca")
+        forca.jogar()
+    elif jogo == 2:
+        print("Jogando Adivinhação")
+        adivinhacao.jogar()
+
+if (__name__ == "__main__"):
+    escolha_jogos()
+
