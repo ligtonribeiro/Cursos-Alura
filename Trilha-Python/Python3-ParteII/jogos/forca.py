cb7abf7753fb088f10bd8225740b5ea5ae432fc6
Wellington Menezes <ligton.ribeiro@gmail.com>
2020-05-23 02:08:07 -0300

    Inicio jogo da forca

--- Trilha-Python/Python3-ParteII/jogos/forca.py
+++ Trilha-Python/Python3-ParteII/jogos/forca.py
@@ -1,11 +1,17 @@
 def jogar():
     print("*********************************")
-    print("Bem vindo ao jogo da Forca!")
+    print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
+    palavra_secreta = "laranja"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
 
     print("Fim do jogo!")
 
 
 if __name__ == "__main__":
     jogar()
-
