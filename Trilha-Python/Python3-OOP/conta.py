4a6573cf0d9a8cdd4d3aa803afead10abe409e3e
Wellington Menezes <ligton.ribeiro@gmail.com>
2020-05-29 23:08:06 -0300

    Atualização arquivo conta.py.

--- Trilha-Python/Python3-OOP/conta.py
+++ Trilha-Python/Python3-OOP/conta.py
@@ -1,27 +1,36 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
-
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
-
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def depositar(self, valor):
-        self.saldo += valor
-
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+    def get_saldo(self):
+        return print(self.__saldo)
+
+    def get_titular(self):
+        return print(self.__titular)
+
+    def get_limite(self):
+        return print(self.__limite)
+
+    def set_limite(self, limite):
+        self.__limite = limite
 
 
-conta = Conta(123, "Wellington", 55.0, 1000.0)
-conta.extrato()
-conta.depositar(100.0)
-conta.extrato()
-conta.sacar(30.0)
-conta.extrato()
-
+conta1 = Conta(123, "Wellinton", 55.0, 1000.0)
+conta1.set_limite(10000)
+conta1.get_limite()
