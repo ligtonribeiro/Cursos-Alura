9bf305e74168374a4de9de2a28bd717c4f2f0683
Wellington Menezes <ligton.ribeiro@gmail.com>
2020-06-17 00:15:31 -0300

    Atualização dos arquivos do curso

---
+++ JavaScript-OO/bytebank/ContaPoupanca.js
@@ -0,0 +1,9 @@
+export class ContaPoupanca{
+    constructor(saldoInicial, cliente, agencia){
+        this._saldoInicial = saldoInicial;
+        this.s_cliente = cliente;
+        this._agencia = agencia;
+    }
+
+
+}
