a5a732b482db62e2f78ecba30be47528a0090355
Wellington Menezes <ligton.ribeiro@gmail.com>
2020-06-19 01:04:52 -0300

    Inicio curso Javascript Padrões de Projeto

--- JavaScript-OO/bytebank/ContaPoupanca.js
+++
@@ -1,9 +0,0 @@
-export class ContaPoupanca{
-    constructor(saldoInicial, cliente, agencia){
-        this._saldoInicial = saldoInicial;
-        this.s_cliente = cliente;
-        this._agencia = agencia;
-    }
-
-
-}
