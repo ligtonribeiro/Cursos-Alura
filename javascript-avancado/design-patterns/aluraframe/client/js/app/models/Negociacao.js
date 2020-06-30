977d793a453dc25142373c0ff21a08141723094f
Wellington Menezes <ligton.ribeiro@gmail.com>
2020-06-30 08:05:06 -0300

    Criação NegociacaoController.js

--- javascript-avancado/design-patterns/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado/design-patterns/aluraframe/client/js/app/models/Negociacao.js
@@ -1,23 +1,24 @@
 class Negociacao {
     constructor(data, quantidade, valor) {
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
-    getVolume() {
-        return this._quantidade * this.valor;
+    get volume() {
+        return this._quantidade * this._valor;
     }
 
-    getData() {
-        return this._data;
+    get data() {
+        return new Date(this._data.getTime());
     }
 
-    getQuantidade() {
+    get quantidade() {
         return this._quantidade;
     }
 
-    getValor() {
+    get valor() {
         return this._valor;
     }
 }
