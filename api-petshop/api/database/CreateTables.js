a2fa601d0c09f01fdd6d09d560d27a149a5dc000
Wellington Menezes <ligton.ribeiro@gmail.com>
2022-07-08 07:20:51 -0300

    ajustes falha de segurança

--- api-petshop/api/database/CreateTables.js
+++
@@ -1,6 +0,0 @@
-const ModelTable = require('../routers/fornecedores/ModelTableFornecedores');
-
-ModelTable
-    .sync()
-    .then(() => console.log('Tabela criada com sucesso!'))
-    .catch(console.log);
--- api-petshop/api/erros/ValorNaoSuportado.js
+++
@@ -1,9 +0,0 @@
-class ValorNaoSuportado extends Error {
-    constructor (contentType) {
-        super(`O tipo de conteúdo ${contentType} não é suportado`)
-        this.name = 'Valor não suportado'
-        this.idErro = 3
-    }
-}
-
-module.exports = ValorNaoSuportado
